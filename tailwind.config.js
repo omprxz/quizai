807593621e416cc39870cd809365b7d0fd45c1e6
Om <omprxz@gmail.com>
2024-08-13 16:47:09 +0530

    Added daisyui themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,12 +19,12 @@ module.exports = {
     ],
     daisyui: {
         themes: [
-            "light", "dark"/*, "autumn", "cupcake", "bumblebee", "emerald", 
+            "light", "dark", "autumn", "cupcake", "bumblebee", "emerald", 
             "corporate", "synthwave", "retro", "cyberpunk", "valentine", 
             "halloween", "garden", "forest", "aqua", "lofi", "pastel", 
             "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", 
             "business", "acid", "lemonade", "night", "coffee", "winter", 
             "dim", "nord", "sunset"
-        */]
+        ]
     }
 };
