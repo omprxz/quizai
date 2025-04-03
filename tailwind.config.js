c74e6d8301a7ccc483312063d4fd4718809cfac1
Om <omprxz@gmail.com>
2025-04-03 09:59:09 +0530

    Enable daisyUI plugin and configure themes in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,19 +8,19 @@ module.exports = {
             }
         }
     },
-    // plugins: [
-    //     require("daisyui"),
-    // ],
+    plugins: [
+        require("daisyui"),
+    ],
     darkMode: ['class', '[data-theme="luxury"]'],
-    // daisyui: {
-    //     themes: [
-    //         "autumn", {
-    //           "luxury": {
-    //             ...require("daisyui/src/theming/themes")["luxury"],
-    //             error: "hsl(0, 84.2%, 60.2%)",
-    //           }
-    //         }, "light", "dark", "coffee", "corporate", "retro", "black", "valentine", "night"
-    //     ],
-    //     darkTheme: "luxury",
-    // }
+    daisyui: {
+        themes: [
+            "autumn", {
+              "luxury": {
+                ...require("daisyui/src/theming/themes")["luxury"],
+                error: "hsl(0, 84.2%, 60.2%)",
+              }
+            }, "light", "dark", "coffee", "corporate", "retro", "black", "valentine", "night"
+        ],
+        darkTheme: "luxury",
+    }
 };
